6dff5cdd86ac2db79380ec0c65d48b54a5d84d23
Joerg Boeselt <joerg@brightcookie.com.au>
2014-06-02 13:14:06 +0930

    first one

---
+++ JavaScript/TimeStampToUri.js
@@ -0,0 +1,13 @@
+/**
+ * Adds a timestamp to a given uri, 
+ * only valid cross browser solution for bypassing browser cache for XMLHttpRequest
+ * @param string uri
+ * @return string
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Using_XMLHttpRequest#Cross-site_XMLHttpRequest
+ */
+
+uniqueURI = function(uri){
+    return uri + ((/\?/).test(uri) ? "&" : "?") + (new Date()).getTime();
+};
+console.log(uniqueURI('https://www.google.com/search?q=test'));
+console.log(uniqueURI('https://www.google.com'));
