9eb0de0b233016231702887cae20b37d3bb1ae4d
Joerg Boeselt <joerg@brightcookie.com.au>
2015-02-17 11:06:48 +1030

    rawurlencode for PHP - JS cookies

--- JavaScript/jsonCookies.js
+++ JavaScript/jsonCookies.js
@@ -5,34 +5,39 @@
  */
 var Cookie = {
 
-	set : function(name, value, expiryDate) {
-		//accepts javascript date objects or already parsed tdate sttrings (not validated!)
-		var expires = '';
-		if ( typeof expiryDate === 'object' && expiryDate instanceof Date) {
-			expires = expiryDate.toGMTString();
-		}
-		if (expires) {
-			expires = ';expires=' + expires;
-		}
-		var cookie = [name, '=', JSON.stringify(value), expires, '; domain=.', window.location.host.toString(), '; path=/;'].join('');
-		document.cookie = cookie;
-	},
+    set : function(name, value, expiryDate) {
+        //accepts javascript date objects or already parsed tdate sttrings (not validated!)
+        var expires = '';
+        if ( typeof expiryDate === 'object' && expiryDate instanceof Date) {
+            expires = expiryDate.toGMTString();
+        }
+        if (expires) {
+            expires = ';expires=' + expires;
+        }
+        var cookie = [name, '=', JSON.stringify(value), expires, '; domain=.', window.location.host.toString(), '; path=/;'].join('');
+        document.cookie = cookie;
+    },
 
-	get : function(name) {
-		var result = document.cookie.match(new RegExp(name + '=([^;]+)'));
-		result = (result) ? JSON.parse(result[1]) : null;
-		return result;
-	},
+    get : function(name) {
+        var result = document.cookie.match(new RegExp(name + '=([^;]+)'));
+        result = (result) ? JSON.parse(result[1]) : null;
+        return result;
+    },
 
-	del : function() {
-		var cookie = [name, '=; expires=Thu, 01-Jan-1970 00:00:01 GMT; path=/; domain=.', window.location.host.toString()].join('');
-		document.cookie = cookie;
-	}
+    del : function() {
+        var cookie = [name, '=; expires=Thu, 01-Jan-1970 00:00:01 GMT; path=/; domain=.', window.location.host.toString()].join('');
+        document.cookie = cookie;
+    }
 };
 
 //// Example:
 // Set expiry date for one year
-//var ex = new Date();
-//ex.setFullYear(new Date().getFullYear() + 1);
-//Cookie.set('test', {test:1}, ex);
-//console.log(Cookie.get('test'));
+// var ex = new Date();
+// ex.setFullYear(new Date().getFullYear() + 1);
+// Cookie.set('test', {test:1}, ex);
+// console.log(Cookie.get('test'));
+
+//// PHP
+// Set cookie with RAW urlencoding in PHP so it can be read via JavaScript
+// @see http://stackoverflow.com/a/2678602
+// $cookie = setrawcookie($name, rawurlencode(json_encode($date)), $expiresAt);
