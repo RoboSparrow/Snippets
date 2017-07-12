2b1c681bfab5d27be22ed1229087476b794f4b7d
Joerg Boeselt <RoboSparrow@users.noreply.github.com>
2017-07-12 17:10:53 +0930

    Because you'd be in jail

--- JavaScript/parseObjectByString.js
+++ JavaScript/parseObjectByString.js
@@ -1,6 +1,9 @@
+
+// 1) /////////////////////////////////////////////
 /**
  * Very cool script by Alnitak, parses nested properties from string
  * @see: Object.byString(someObj, 'part3[0].name');  
+ * @see https://stackoverflow.com/a/6491621
  */ 
  
 function parseObjectByString(o, s) {
@@ -20,4 +23,48 @@ function parseObjectByString(o, s) {
     }
     return o;
 }
-// var val = parseObjectByString(someObj, 'part3[0].name');
+
+var val = undefined;
+try {
+    var val = parseObjectByString(someObj, 'part3[0].name');
+} catch (e) {
+ /*nothing*/
+}
+
+// 2) /////////////////////////////////////////////
+
+/**
+ * dot notation (no array)
+ * @see https://stackoverflow.com/questions/6393943/convert-javascript-string-in-dot-notation-into-an-object-reference
+ */
+ 
+'use strict';
+
+const assert = require('assert');
+
+const  obj = {
+    a: {
+        b: '1',
+        c: '2' ,
+        d: {
+            etc: 'etc1'
+        },
+        etc:'etc2'
+    },
+    b: '3',
+    etc:'etc3'
+};
+
+let res;
+
+res = 'a.b.c'.split('.').reduce((o,i) => { return o[i] }, obj);
+assert.strictEqual(res, undefined);
+
+res = 'etc'.split('.').reduce((o,i) => { return o[i] }, obj);
+assert.strictEqual(res, 'etc3');
+
+res = 'a.c'.split('.').reduce((o,i) => { return o[i] }, obj);
+assert.strictEqual(res, '2');
+
+res = 'a.d.etc'.split('.').reduce((o,i) => { return o[i] }, obj);
+assert.strictEqual(res, 'etc1');
